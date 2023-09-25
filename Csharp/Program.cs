27fb2f9a309231c2ecb087c732eea177668a9c88
Daulet <daulet@qit.kz>
2023-09-25 09:38:34 +0600

    commit

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Lesson {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            int a, b;
+
+            Console.WriteLine("Введите первое число");
+
+            str = Console.ReadLine();
+
+            a=Convert.ToInt32(str);
+
+            Console.WriteLine("ВВедите второе число");
+
+            str=Console.ReadLine();
+
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+
+            Console.WriteLine("Сумма чисел" + result);
+        }
+
+    }
+
+}
+
+
+
