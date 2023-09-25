8acd24decf85fb33f5738fa64e6018c7db3e9a05
Daulet <daulet@qit.kz>
2023-09-25 10:05:14 +0600

    lesson 11  increment

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -5,25 +5,11 @@ namespace Lesson {
     {
         static void Main(string[] args)
         {
-            string str;
+            int a = 1;
 
-            int a, b;
+            a = a++ * a;
 
-            Console.WriteLine("Введите первое число");
-
-            str = Console.ReadLine();
-
-            a=Convert.ToInt32(str);
-
-            Console.WriteLine("ВВедите второе число");
-
-            str=Console.ReadLine();
-
-            b = Convert.ToInt32(str);
-
-            int result = a + b;
-
-            Console.WriteLine("Сумма чисел" + result);
+            Console.WriteLine(a);
         }
 
     }
