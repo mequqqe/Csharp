2a21b632f88aec07f79cadedbbe753ba505b01c3
Daulet <daulet@qit.kz>
2023-09-26 11:06:53 +0600

    while

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -5,35 +5,13 @@ namespace Lesson {
     {
         static void Main(string[] args)
         {
-            double firstValue, secondValue;
-            string action;
-
-            Console.WriteLine("Введите число 1");
-            firstValue = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("ВВедите число 2");
-            secondValue = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Выберите операцию '+' '- '*' '/' ");
-            action = Console.ReadLine();
-
-            switch (action) 
+            int count = 0;
+           
+            int limit = int.Parse(Console.ReadLine());
+            while(count < limit) 
             {
-                case "+":
-                    Console.WriteLine(firstValue + secondValue);
-                    break;
-                case "-":
-                    Console.WriteLine(firstValue - secondValue);
-                    break;
-                case "*":
-                    Console.WriteLine(firstValue / secondValue);
-                    break;
-                    case "/":
-                        Console.WriteLine(firstValue / secondValue);
-                    break;
-                default:
-                    Console.WriteLine("Ошибка");
-                    break;
+                count++;
+                Console.WriteLine(count);
             }
         }
 
